3c220b2cc5daf480b111ccd75be20e93ec6bee1e
cruz-f <fernandocruz184@gmail.com>
2022-01-20 11:56:31 +0000

    [ADD] complete of base properties details

--- data/base.py
+++ data/base.py
@@ -8,9 +8,9 @@ class BaseNode(DjangoNode):
     __abstract_node__ = True
 
     uid = UniqueIdProperty()
-    protrend_id = StringProperty(required=True, unique_index=True)
-    created = DateTimeProperty(default_now=True)
-    updated = DateTimeProperty(default_now=True)
+    protrend_id = StringProperty(required=True, unique_index=True, help_text=help_text.protrend_id)
+    created = DateTimeProperty(default_now=True, help_text=help_text.created)
+    updated = DateTimeProperty(default_now=True, help_text=help_text.updated)
 
     class Meta:
         app_label = 'data'
