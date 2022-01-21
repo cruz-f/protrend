2bdbc86435627c7f0f300518e635a7c09268a0f2
cruz-f <fernandocruz184@gmail.com>
2022-01-21 15:05:29 +0000

    [ADD] adding the concrete api views for the effector evidence nodes

--- interfaces/api/views.py
+++ interfaces/api/views.py
@@ -1,24 +1,33 @@
-from typing import Type, Callable, List, Union
+from abc import abstractmethod
+from typing import Type, List, Union
 
 from django.db.models import Model
 from django_neomodel import DjangoNode
-from rest_framework import status
+from neomodel import NeomodelException
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.serializers import Serializer
 
+import domain.database as papi
+import interfaces.api.serializers as serializers
 from .permissions import SuperUserOrReadOnly
 
 
+# --------------------------------------------
+# BASE API VIEWS
+# --------------------------------------------
 class ObjectList:
     """
     View to list and create ProTReND database objects.
     """
     serializer_class: Type[Serializer] = None
-    queryset: Callable = None
     permission_classes = [SuperUserOrReadOnly]
 
-    def get_queryset(self) -> List[DjangoNode, Model]:
-        return self.queryset()
+    @abstractmethod
+    def get_queryset(self) -> Union[List[DjangoNode], List[Model]]:
+        pass
 
     def get(self, request):
         objs = self.get_queryset()
@@ -28,8 +37,13 @@ class ObjectList:
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            except NeomodelException as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -38,16 +52,16 @@ class ObjectDetail:
     View to retrieve, update and delete ProTReND database objects.
     """
     serializer_class: Type[Serializer] = None
-    queryset: Callable = None
     permission_classes = [SuperUserOrReadOnly]
 
-    def get_queryset(self, protrend_id) -> Union[DjangoNode, Model]:
-        return self.queryset(protrend_id)
+    @abstractmethod
+    def get_queryset(self, protrend_id: str) -> Union[DjangoNode, Model]:
+        pass
 
-    def get_object(self, protrend_id):
+    def get_object(self, protrend_id: str):
         return self.get_queryset(protrend_id)
 
-    def get(self, request, protrend_id):
+    def get(self, request, protrend_id: str):
         obj = self.get_object(protrend_id)
         if obj is None:
             return Response({}, status=status.HTTP_404_NOT_FOUND)
@@ -55,7 +69,7 @@ class ObjectDetail:
         serializer = self.serializer_class(obj)
         return Response(serializer.data)
 
-    def put(self, request, protrend_id):
+    def put(self, request, protrend_id: str):
         obj = self.get_object(protrend_id)
         serializer = self.serializer_class(obj, data=request.data)
         if serializer.is_valid():
@@ -67,3 +81,41 @@ class ObjectDetail:
         obj = self.get_object(protrend_id)
         obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# --------------------------------------------
+# CONCRETE API VIEWS
+# --------------------------------------------
+@api_view(['GET'])
+def api_root(request):
+    data = {'effectors': reverse('effector-list', request=request),
+            'evidence': reverse('evidence-list', request=request)}
+    return Response(data)
+
+
+class EffectorList(ObjectList, generics.GenericAPIView):
+    serializer_class = serializers.EffectorSerializer
+
+    def get_queryset(self):
+        return papi.get_effectors()
+
+
+class EffectorDetail(ObjectDetail, generics.GenericAPIView):
+    serializer_class = serializers.EffectorSerializer
+
+    def get_queryset(self, protrend_id: str):
+        return papi.get_effector_by_id(protrend_id)
+
+
+class EvidenceList(ObjectList, generics.GenericAPIView):
+    serializer_class = serializers.EvidenceSerializer
+
+    def get_queryset(self):
+        return papi.get_evidences()
+
+
+class EvidenceDetail(ObjectDetail, generics.GenericAPIView):
+    serializer_class = serializers.EvidenceSerializer
+
+    def get_queryset(self, protrend_id: str):
+        return papi.get_evidence_by_id(protrend_id)
