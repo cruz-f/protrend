4ce3f09957d0277d9f8a30dd0a5fb4fbf23e4c24
cruz-f <fernandocruz184@gmail.com>
2022-01-21 15:06:52 +0000

    [FIX] fixing default required=true of the django-rest-framework fields

--- interfaces/api/serializers.py
+++ interfaces/api/serializers.py
@@ -10,9 +10,9 @@ from constants import help_text, choices
 # --------------------------------------
 class BaseSerializer(serializers.Serializer):
     # properties
-    protrend_id = serializers.CharField(read_only=True, unique_index=True, help_text=help_text.protrend_id)
-    created = serializers.DateTimeField(read_only=True, default_now=True, help_text=help_text.created)
-    updated = serializers.DateTimeField(default_now=True, help_text=help_text.updated)
+    protrend_id = serializers.CharField(read_only=True, help_text=help_text.protrend_id)
+    created = serializers.DateTimeField(read_only=True, help_text=help_text.created)
+    updated = serializers.DateTimeField(required=False, help_text=help_text.updated)
 
     def update(self, instance, validated_data):
         pass
@@ -37,7 +37,7 @@ class NameMixInSerializer(serializers.Serializer):
 
 class SequenceMixInSerializer(serializers.Serializer):
     # properties
-    sequence = serializers.CharField(help_text=help_text.sequence)
+    sequence = serializers.CharField(required=False, help_text=help_text.sequence)
 
     def update(self, instance, validated_data):
         pass
@@ -48,9 +48,9 @@ class SequenceMixInSerializer(serializers.Serializer):
 
 class PositionMixInSerializer(serializers.Serializer):
     # properties
-    strand = serializers.CharField(choices=choices.strand, help_text=help_text.strand)
-    start = serializers.IntegerField(help_text=help_text.start)
-    stop = serializers.IntegerField(help_text=help_text.stop)
+    strand = serializers.ChoiceField(required=False, choices=choices.strand, help_text=help_text.strand)
+    start = serializers.IntegerField(required=False, help_text=help_text.start)
+    stop = serializers.IntegerField(required=False, help_text=help_text.stop)
 
     def update(self, instance, validated_data):
         pass
@@ -62,15 +62,16 @@ class PositionMixInSerializer(serializers.Serializer):
 class GeneMixInSerializer(serializers.Serializer):
     # properties
     locus_tag = serializers.CharField(required=True, max_length=50, help_text=help_text.locus_tag)
-    uniprot_accession = serializers.CharField(max_length=50, help_text=help_text.uniprot_accession)
-    name = serializers.CharField(max_length=50, help_text=help_text.gene_name)
-    synonyms = serializers.ListField(child=serializers.CharField(), help_text=help_text.synonyms)
-    function = serializers.CharField(help_text=help_text.function)
-    description = serializers.CharField(help_text=help_text.description)
-    ncbi_gene = serializers.IntegerField(max_length=50, help_text=help_text.ncbi_gene)
-    ncbi_protein = serializers.IntegerField(max_length=50, help_text=help_text.ncbi_protein)
-    genbank_accession = serializers.CharField(max_length=50, help_text=help_text.genbank_accession)
-    refseq_accession = serializers.CharField(max_length=50, help_text=help_text.refseq_accession)
+    uniprot_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.uniprot_accession)
+    name = serializers.CharField(required=False, max_length=50, help_text=help_text.gene_name)
+    synonyms = serializers.ListField(required=False, child=serializers.CharField(required=False),
+                                     help_text=help_text.synonyms)
+    function = serializers.CharField(required=False, help_text=help_text.function)
+    description = serializers.CharField(required=False, help_text=help_text.description)
+    ncbi_gene = serializers.IntegerField(required=False, help_text=help_text.ncbi_gene)
+    ncbi_protein = serializers.IntegerField(required=False, help_text=help_text.ncbi_protein)
+    genbank_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.genbank_accession)
+    refseq_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.refseq_accession)
 
     def update(self, instance, validated_data):
         pass
@@ -84,7 +85,9 @@ class GeneMixInSerializer(serializers.Serializer):
 # --------------------------------------
 class EffectorSerializer(BaseSerializer, NameMixInSerializer):
     # properties
-    kegg_compounds = serializers.ListField(child=serializers.CharField(), help_text=help_text.kegg_compounds)
+    kegg_compounds = serializers.ListField(child=serializers.CharField(required=False),
+                                           required=False,
+                                           help_text=help_text.kegg_compounds)
 
     # # relationships
     # data_source = RelationshipTo('.source.Source', BASE_REL_TYPE, model=SourceRelationship)
@@ -101,7 +104,7 @@ class EffectorSerializer(BaseSerializer, NameMixInSerializer):
 
 class EvidenceSerializer(BaseSerializer, NameMixInSerializer):
     # properties
-    description = serializers.CharField(help_text=help_text.generic_description)
+    description = serializers.CharField(required=False, help_text=help_text.generic_description)
 
     # # relationships
     # regulator = RelationshipTo('.regulator.Regulator', BASE_REL_TYPE, model=BaseRelationship)
@@ -145,9 +148,10 @@ class OperonSerializer(BaseSerializer, PositionMixInSerializer):
 
     # properties
     operon_db_id = serializers.CharField(required=True, max_length=50, help_text=help_text.operon_db_id)
-    name = serializers.CharField(max_length=50, help_text=help_text.operon_name)
-    function = serializers.CharField(max_length=250, help_text=help_text.operon_function)
-    genes = serializers.ListField(child=serializers.CharField(), required=True, help_text=help_text.operon_genes)
+    name = serializers.CharField(required=False, max_length=50, help_text=help_text.operon_name)
+    function = serializers.CharField(required=False, max_length=250, help_text=help_text.operon_function)
+    genes = serializers.ListField(required=True, child=serializers.CharField(required=True),
+                                  help_text=help_text.operon_genes)
 
     # # relationships
     # data_source = RelationshipTo('.source.Source', BASE_REL_TYPE, cardinality=One, model=SourceRelationship)
@@ -166,15 +170,15 @@ class OperonSerializer(BaseSerializer, PositionMixInSerializer):
 class OrganismSerializer(BaseSerializer):
     # properties
     name = serializers.CharField(required=True, max_length=200, help_text=help_text.organism_name)
-    ncbi_taxonomy = serializers.IntegerField(help_text=help_text.ncbi_taxonomy)
-    species = serializers.CharField(max_length=150, help_text=help_text.species)
-    strain = serializers.CharField(max_length=150, help_text=help_text.strain)
-    refseq_accession = serializers.CharField(max_length=50, help_text=help_text.refseq_accession)
-    refseq_ftp = serializers.CharField(max_length=250, help_text=help_text.refseq_ftp)
-    genbank_accession = serializers.CharField(max_length=50, help_text=help_text.genbank_accession)
-    genbank_ftp = serializers.CharField(max_length=250, help_text=help_text.genbank_ftp)
-    ncbi_assembly = serializers.IntegerField(help_text=help_text.ncbi_assembly)
-    assembly_accession = serializers.CharField(max_length=50, help_text=help_text.assembly_accession)
+    ncbi_taxonomy = serializers.IntegerField(required=False, help_text=help_text.ncbi_taxonomy)
+    species = serializers.CharField(required=False, max_length=150, help_text=help_text.species)
+    strain = serializers.CharField(required=False, max_length=150, help_text=help_text.strain)
+    refseq_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.refseq_accession)
+    refseq_ftp = serializers.CharField(required=False, max_length=250, help_text=help_text.refseq_ftp)
+    genbank_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.genbank_accession)
+    genbank_ftp = serializers.CharField(required=False, max_length=250, help_text=help_text.genbank_ftp)
+    ncbi_assembly = serializers.IntegerField(required=False, help_text=help_text.ncbi_assembly)
+    assembly_accession = serializers.CharField(required=False, max_length=50, help_text=help_text.assembly_accession)
 
     # # relationships
     # data_source = RelationshipTo('.source.Source', BASE_REL_TYPE, model=SourceRelationship)
@@ -193,7 +197,8 @@ class OrganismSerializer(BaseSerializer):
 
 class PathwaySerializer(BaseSerializer, NameMixInSerializer):
     # properties
-    kegg_pathways = serializers.ListField(child=serializers.CharField(), help_text=help_text.kegg_pathways)
+    kegg_pathways = serializers.ListField(required=False, child=serializers.CharField(required=False), allow_empty=True,
+                                          help_text=help_text.kegg_pathways)
 
     # # relationships
     # data_source = RelationshipTo('.source.Source', BASE_REL_TYPE, model=SourceRelationship)
@@ -209,10 +214,10 @@ class PathwaySerializer(BaseSerializer, NameMixInSerializer):
 class Publication(BaseSerializer):
     # properties
     pmid = serializers.IntegerField(required=True, help_text=help_text.pmid)
-    doi = serializers.CharField(max_length=250, help_text=help_text.doi)
-    title = serializers.CharField(max_length=500, help_text=help_text.title)
-    author = serializers.CharField(max_length=250, help_text=help_text.author)
-    year = serializers.IntegerField(help_text=help_text.year)
+    doi = serializers.CharField(required=False, max_length=250, help_text=help_text.doi)
+    title = serializers.CharField(required=False, max_length=500, help_text=help_text.title)
+    author = serializers.CharField(required=False, max_length=250, help_text=help_text.author)
+    year = serializers.IntegerField(required=False, help_text=help_text.year)
 
     # # relationships
     # regulatory_family = RelationshipTo('.regulatory_family.RegulatoryFamily', BASE_REL_TYPE, model=BaseRelationship)
@@ -231,8 +236,8 @@ class Publication(BaseSerializer):
 
 class RegulatorSerializer(BaseSerializer, GeneMixInSerializer, SequenceMixInSerializer, PositionMixInSerializer):
     # properties
-    mechanism = serializers.CharField(required=True, choices=choices.mechanism,
-                                      help_text=help_text.mechanism)
+    mechanism = serializers.ChoiceField(required=False, choices=choices.mechanism,
+                                        help_text=help_text.mechanism)
 
     # properties inherited from GeneMixInSerializer, SequenceMixInSerializer, PositionMixInSerializer
 
@@ -258,8 +263,8 @@ class RegulatorSerializer(BaseSerializer, GeneMixInSerializer, SequenceMixInSeri
 
 class RegulatoryFamilySerializer(BaseSerializer, NameMixInSerializer):
     # properties
-    mechanism = serializers.CharField(required=True, choices=choices.mechanism,
-                                      help_text=help_text.mechanism)
+    mechanism = serializers.ChoiceField(required=False, choices=choices.mechanism,
+                                        help_text=help_text.mechanism)
     rfam = serializers.CharField(max_length=100, help_text=help_text.rfam)
     description = serializers.CharField(help_text=help_text.generic_description)
 
@@ -280,10 +285,10 @@ class RegulatoryInteraction(BaseSerializer):
     organism = serializers.CharField(required=True, max_length=100, help_text=help_text.organism_id)
     regulator = serializers.CharField(required=True, max_length=100, help_text=help_text.regulator_id)
     gene = serializers.CharField(required=True, max_length=100, help_text=help_text.gene_id)
-    tfbs = serializers.CharField(max_length=100, help_text=help_text.tfbs_id)
-    effector = serializers.CharField(max_length=100, help_text=help_text.effector_id)
-    regulatory_effect = serializers.CharField(required=True, choices=choices.regulatory_effect,
-                                              help_text=help_text.regulatory_effect)
+    tfbs = serializers.CharField(required=False, max_length=100, help_text=help_text.tfbs_id)
+    effector = serializers.CharField(required=False, max_length=100, help_text=help_text.effector_id)
+    regulatory_effect = serializers.ChoiceField(required=True, choices=choices.regulatory_effect,
+                                                help_text=help_text.regulatory_effect)
 
     # # relationships
     # data_source = RelationshipTo('.source.Source', BASE_REL_TYPE, model=SourceRelationship)
