e30efa539d3235b3b533baabf9a4a069462e76e7
cruz-f <fernandocruz184@gmail.com>
2022-01-21 10:38:54 +0000

    [ADD] adding transformers to process common unique indexes

---
+++ transformers.py
@@ -0,0 +1,21 @@
+from typing import Callable, Any
+
+
+def apply_transformers(item: Any, *transformers: Callable) -> Any:
+    for transformer in transformers:
+        item = transformer(item)
+
+    return item
+
+
+def lower(item: str) -> str:
+    return item.lower()
+
+
+def rstrip(item: str) -> str:
+    return item.rstrip()
+
+
+def lstrip(item: str) -> str:
+    return item.lstrip()
+
