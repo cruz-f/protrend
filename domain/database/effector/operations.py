de695ac52f480bef4e77d71b0bd8c345092423ed
cruz-f <fernandocruz184@gmail.com>
2022-01-21 20:05:13 +0000

    [ADD] adding name validators for args (bulk creation) and kwargs (single creation)

--- domain/database/effector/operations.py
+++ domain/database/effector/operations.py
@@ -5,36 +5,18 @@ from rest_framework import status
 import domain.model_api as mapi
 from data import Effector
 from exceptions import ProtrendException
-from transformers import lstrip, rstrip, lower, to_str
-from .._sanity import _sanitize_duplicates, _sanitize_protrend_idx, _sanitize_factor
-from ..utils import protrend_id_encoder
+from .._validate import _validate_kwargs_by_name, _validate_args_by_name
+
+
+_HEADER = 'PRT'
+_ENTITY = 'EFC'
 
 
 def create_effectors(*effectors: Dict[str, Any]) -> List[Effector]:
     """
     Create effectors into the database
     """
-    for effector in effectors:
-        obj = _sanitize_duplicates(value=effector['name'],
-                                   transformers=[to_str, lower, rstrip, lstrip],
-                                   node_cls=Effector,
-                                   key='name_factor')
-
-        if obj is not None:
-            raise ProtrendException(detail=f'Object with name {obj.name} already exists in the database and '
-                                           f'has the following protrend_id: {obj.protrend_id}',
-                                    code='create error',
-                                    status=status.HTTP_400_BAD_REQUEST)
-    idx = _sanitize_protrend_idx(Effector)
-
-    for i, effector in enumerate(effectors):
-        i = i + idx + 1
-        new_id = protrend_id_encoder(header='PRT', entity='RFC', integer=i)
-        effector['protrend_id'] = new_id
-
-        new_factor = _sanitize_factor(value=effector['name'], transformers=[to_str, lower, rstrip, lstrip])
-        effector['name_factor'] = new_factor
-
+    effectors = _validate_args_by_name(args=effectors, node_cls=Effector, header=_HEADER, entity=_ENTITY)
     return mapi.create_objects(Effector, *effectors)
 
 
@@ -49,24 +31,7 @@ def create_effector(**kwargs) -> Effector:
     """
     Create a given effector into the database according to the parameters
     """
-    obj = _sanitize_duplicates(value=kwargs['name'],
-                               transformers=[to_str, lower, rstrip, lstrip],
-                               node_cls=Effector,
-                               key='name_factor')
-
-    if obj is not None:
-        raise ProtrendException(detail=f'Object with name {obj.name} already exists in the database and '
-                                       f'has the following protrend_id: {obj.protrend_id}',
-                                code='create error',
-                                status=status.HTTP_400_BAD_REQUEST)
-
-    idx = _sanitize_protrend_idx(Effector)
-    new_id = protrend_id_encoder(header='PRT', entity='EFC', integer=idx)
-    kwargs['protrend_id'] = new_id
-
-    new_factor = _sanitize_factor(value=kwargs['name'], transformers=[to_str, lower, rstrip, lstrip])
-    kwargs['name_factor'] = new_factor
-
+    kwargs = _validate_kwargs_by_name(kwargs=kwargs, node_cls=Effector, header=_HEADER, entity=_ENTITY)
     return mapi.create_object(Effector, **kwargs)
 
 
@@ -76,23 +41,13 @@ def update_effector(effector: Effector, **kwargs) -> Effector:
     """
     if 'protrend_id' in kwargs:
         raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
-                                code='update error',
+                                code='create or update error',
                                 status=status.HTTP_400_BAD_REQUEST)
 
     if 'name' not in kwargs:
         return mapi.update_object(effector, **kwargs)
 
-    obj = _sanitize_duplicates(value=kwargs['name'],
-                               transformers=[to_str, lower, rstrip, lstrip],
-                               node_cls=Effector,
-                               key='name_factor')
-
-    if obj is not None:
-        raise ProtrendException(detail=f'Object with name {obj.name} already exists in the database and '
-                                       f'has the following protrend_id: {obj.protrend_id}. Please create a new one',
-                                code='update error',
-                                status=status.HTTP_400_BAD_REQUEST)
-
+    _ = _validate_kwargs_by_name(kwargs=kwargs, node_cls=Effector, header=_HEADER, entity=_ENTITY)
     return mapi.update_object(effector, **kwargs)
 
 
