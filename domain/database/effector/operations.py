ac2f6b218483c85ef71e2c2d2f94f03449366cbd
cruz-f <fernandocruz184@gmail.com>
2022-01-21 17:56:36 +0000

    [FIX] fixing minor issues in the operations of Effectors [ADD] raising custom protrend exceptions

--- domain/database/effector/operations.py
+++ domain/database/effector/operations.py
@@ -1,9 +1,8 @@
 from typing import List, Dict, Any
 
-from django.core.exceptions import PermissionDenied
-
 import domain.model_api as mapi
 from data import Effector
+from exceptions import ProtrendException
 from transformers import lstrip, rstrip, lower, apply_transformers
 from ..utils import protrend_id_decoder, protrend_id_encoder, protrend_identifiers_batch
 
@@ -21,11 +20,17 @@ def create_effectors(*effectors: Dict[str, Any]) -> List[Effector]:
     for obj in current_objs:
         obj_name = apply_transformers(obj.name, lower, rstrip, lstrip)
         if obj_name in names:
-            raise PermissionDenied(f'Object with name {obj_name} already exists in the database and '
-                                   f'has the following protrend_id: {obj.protrend_id}')
+            raise ProtrendException(detail=f'Object with name {obj_name} already exists in the database and '
+                                    f'has the following protrend_id: {obj.protrend_id}',
+                                    code='create error')
 
-    last_obj = current_objs[-1]
-    idx = protrend_id_decoder(last_obj.protrend_id) + 1
+    if current_objs:
+        last_obj = current_objs[-1]
+        idx = protrend_id_decoder(last_obj.protrend_id) + 1
+
+    else:
+        idx = 1
+
     size = len(effectors)
     new_ids = protrend_identifiers_batch(header='PRT', entity='EFC', start=idx, size=size)
 
@@ -53,11 +58,17 @@ def create_effector(**kwargs) -> Effector:
     for obj in current_objs:
         obj_name = apply_transformers(obj.name, lower, rstrip, lstrip)
         if name == obj_name:
-            raise PermissionDenied(f'Object with name {submitted_name} already exists in the database and '
-                                   f'has the following protrend_id: {obj.protrend_id}')
+            raise ProtrendException(detail=f'Object with name {obj_name} already exists in the database and '
+                                           f'has the following protrend_id: {obj.protrend_id}',
+                                    code='create error')
 
-    last_obj = current_objs[-1]
-    idx = protrend_id_decoder(last_obj.protrend_id) + 1
+    if current_objs:
+        last_obj = current_objs[-1]
+        idx = protrend_id_decoder(last_obj.protrend_id) + 1
+
+    else:
+        idx = 1
+
     new_id = protrend_id_encoder(header='PRT', entity='EFC', integer=idx)
     kwargs['protrend_id'] = new_id
     return mapi.create_object(Effector, **kwargs)
@@ -68,7 +79,8 @@ def update_effector(effector: Effector, **kwargs) -> Effector:
     Update the effector into the database according to the parameters
     """
     if 'protrend_id' in kwargs:
-        raise PermissionDenied('Read only attribute: protrend_id field cannot be changed')
+        raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
+                                code='update error')
     return mapi.update_object(effector, **kwargs)
 
 
