cda5afb2b86d3db7e694a937a6c7809fb0333c71
cruz-f <fernandocruz184@gmail.com>
2022-01-25 11:31:05 +0000

    [ADD] adding name validators for args (bulk creation) and kwargs (single creation) of Pathway objects

--- domain/database/pathway/operations.py
+++ domain/database/pathway/operations.py
@@ -1,13 +1,22 @@
 from typing import List, Dict, Any
 
+from rest_framework import status
+
 from data import Pathway
 import domain.model_api as mapi
+from domain.database._validate import _validate_args_by_name, _validate_kwargs_by_name
+from exceptions import ProtrendException
+
+
+_HEADER = 'PRT'
+_ENTITY = 'PTH'
 
 
 def create_pathways(*pathways: Dict[str, Any]) -> List[Pathway]:
     """
     Create pathways into the database
     """
+    pathways = _validate_args_by_name(args=pathways, node_cls=Pathway, header=_HEADER, entity=_ENTITY)
     return mapi.create_objects(Pathway, *pathways)
 
 
@@ -22,6 +31,7 @@ def create_pathway(**kwargs) -> Pathway:
     """
     Create a given pathway into the database according to the parameters
     """
+    kwargs = _validate_kwargs_by_name(kwargs=kwargs, node_cls=Pathway, header=_HEADER, entity=_ENTITY)
     return mapi.create_object(Pathway, **kwargs)
 
 
@@ -29,6 +39,14 @@ def update_pathway(pathway: Pathway, **kwargs) -> Pathway:
     """
     Update the pathway into the database according to the parameters
     """
+    if 'protrend_id' in kwargs:
+        raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
+                                code='create or update error',
+                                status=status.HTTP_400_BAD_REQUEST)
+
+    if 'name' in kwargs:
+        _validate_kwargs_by_name(kwargs=kwargs, node_cls=Pathway, header=_HEADER, entity=_ENTITY)
+
     return mapi.update_object(pathway, **kwargs)
 
 
