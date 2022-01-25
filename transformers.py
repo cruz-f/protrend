5e6f5b832ff292fda5ab9619dfc6fdd0ce64f3ec
cruz-f <fernandocruz184@gmail.com>
2022-01-25 11:25:00 +0000

    [ADD] adding to int and str transformers

--- transformers.py
+++ transformers.py
@@ -1,4 +1,8 @@
+from types import GeneratorType
 from typing import Callable, Any
+
+import numpy as np
+import pandas as pd
 
 
 def apply_transformers(item: Any, *transformers: Callable) -> Any:
@@ -8,8 +12,56 @@ def apply_transformers(item: Any, *transformers: Callable) -> Any:
     return item
 
 
+def is_null(obj: Any) -> bool:
+    # booleans
+    if isinstance(obj, bool):
+        return not obj
+
+    # integers, floats, etc
+    if isinstance(obj, (int, float)):
+        return pd.isnull(obj)
+
+    # numpy arrays
+    if isinstance(obj, np.ndarray):
+        return not obj.any()
+
+    # pandas series and frames
+    if isinstance(obj, (pd.DataFrame, pd.Series)):
+        return obj.empty
+
+    # python built-ins
+    if isinstance(obj, (range, list, tuple, set, dict, frozenset, str)):
+        return len(obj) == 0
+
+    # python generator built-ins
+    if isinstance(obj, GeneratorType):
+        return False
+
+    # pandas check for nan or null
+    if pd.isnull(obj):
+        return True
+
+    return False
+
+
 def to_str(item: Any) -> str:
-    return str(item)
+    if is_null(item):
+        return item
+
+    try:
+        return str(item)
+    except (ValueError, TypeError):
+        return item
+
+
+def to_int(item: Any) -> int:
+    if is_null(item):
+        return item
+
+    try:
+        return int(item)
+    except (ValueError, TypeError):
+        return item
 
 
 def lower(item: str) -> str:
