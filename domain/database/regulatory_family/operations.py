6cfefd6829dff3a9c1a5e1e9d7ccfef58a5ff1ff
cruz-f <fernandocruz184@gmail.com>
2022-01-25 11:45:57 +0000

    [ADD] adding name validators for args (bulk creation) and kwargs (single creation) of RegulatoryFamily objects

--- domain/database/regulatory_family/operations.py
+++ domain/database/regulatory_family/operations.py
@@ -1,13 +1,22 @@
 from typing import List, Dict, Any
 
+from rest_framework import status
+
 from data import RegulatoryFamily
 import domain.model_api as mapi
+from domain.database._validate import _validate_kwargs_by_name, _validate_args_by_name
+from exceptions import ProtrendException
+
+
+_HEADER = 'PRT'
+_ENTITY = 'RFAM'
 
 
 def create_rfams(*rfams: Dict[str, Any]) -> List[RegulatoryFamily]:
     """
     Create rfams into the database
     """
+    rfams = _validate_args_by_name(args=rfams, node_cls=RegulatoryFamily, header=_HEADER, entity=_ENTITY)
     return mapi.create_objects(RegulatoryFamily, *rfams)
 
 
@@ -22,6 +31,7 @@ def create_rfam(**kwargs) -> RegulatoryFamily:
     """
     Create a given rfam into the database according to the parameters
     """
+    kwargs = _validate_kwargs_by_name(kwargs=kwargs, node_cls=RegulatoryFamily, header=_HEADER, entity=_ENTITY)
     return mapi.create_object(RegulatoryFamily, **kwargs)
 
 
@@ -29,6 +39,14 @@ def update_rfam(rfam: RegulatoryFamily, **kwargs) -> RegulatoryFamily:
     """
     Update the rfam into the database according to the parameters
     """
+    if 'protrend_id' in kwargs:
+        raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
+                                code='create or update error',
+                                status=status.HTTP_400_BAD_REQUEST)
+
+    if 'name' in kwargs:
+        _validate_kwargs_by_name(kwargs=kwargs, node_cls=RegulatoryFamily, header=_HEADER, entity=_ENTITY)
+
     return mapi.update_object(rfam, **kwargs)
 
 
