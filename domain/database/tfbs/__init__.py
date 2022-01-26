417e4d847b1db898fddca5fea6a8d3d74e5b81e8
cruz-f <fernandocruz184@gmail.com>
2022-01-26 15:49:45 +0000

    [FIX] renaming of the binding site queries functions in the domain layer

--- domain/database/tfbs/__init__.py
+++ domain/database/tfbs/__init__.py
@@ -1,10 +1,10 @@
-from .queries import (filter_biding_sites,
-                      get_biding_site,
-                      get_biding_site_by_id,
-                      get_biding_sites,
-                      get_last_biding_site,
-                      order_by_biding_sites,
-                      order_biding_sites_by_id)
+from .queries import (filter_binding_sites,
+                      get_binding_site,
+                      get_binding_site_by_id,
+                      get_binding_sites,
+                      get_last_binding_site,
+                      order_by_binding_sites,
+                      order_binding_sites_by_id)
 from .operations import (create_binding_site,
                          create_binding_sites,
                          delete_binding_site,
--- domain/database/tfbs/queries.py
+++ domain/database/tfbs/queries.py
@@ -4,50 +4,50 @@ from data import TFBS
 import domain.model_api as mapi
 
 
-def get_biding_sites() -> List[TFBS]:
+def get_binding_sites() -> List[TFBS]:
     """
-    Get biding_sites list from database
+    Get binding_sites list from database
     """
     return mapi.get_objects(TFBS)
 
 
-def filter_biding_sites(*args, **kwargs) -> List[TFBS]:
+def filter_binding_sites(*args, **kwargs) -> List[TFBS]:
     """
-    Get and filter biding_sites from database
+    Get and filter binding_sites from database
     """
     return mapi.filter_objects(TFBS, *args, **kwargs)
 
 
-def order_by_biding_sites(*fields) -> List[TFBS]:
+def order_by_binding_sites(*fields) -> List[TFBS]:
     """
-    Get and order by biding_sites from database
+    Get and order by binding_sites from database
     """
     return mapi.order_by_objects(TFBS, *fields)
 
 
-def order_biding_sites_by_id() -> List[TFBS]:
+def order_binding_sites_by_id() -> List[TFBS]:
     """
-    Get and order by biding_sites by protrend identifier from database
+    Get and order by binding_sites by protrend identifier from database
     """
     return mapi.order_by_objects(TFBS, 'protrend_id')
 
 
-def get_biding_site(**kwargs) -> TFBS:
+def get_binding_site(**kwargs) -> TFBS:
     """
-    Get a given biding_site from database
+    Get a given binding_site from database
     """
     return mapi.get_object(TFBS, **kwargs)
 
 
-def get_biding_site_by_id(protrend_id: str) -> TFBS:
+def get_binding_site_by_id(protrend_id: str) -> TFBS:
     """
-    Get a given biding_site from database using the protrend identifier
+    Get a given binding_site from database using the protrend identifier
     """
     return mapi.get_object(TFBS, protrend_id=protrend_id)
 
 
-def get_last_biding_site() -> TFBS:
+def get_last_binding_site() -> TFBS:
     """
-    Get the last biding_site from database using the protrend identifier
+    Get the last binding_site from database using the protrend identifier
     """
     return mapi.get_last_object(TFBS, 'protrend_id')
