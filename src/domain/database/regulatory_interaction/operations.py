f833cac4a80e51644cb987c16819138c54c4ae87
cruz-f <fernandocruz184@gmail.com>
2022-02-01 17:26:39 +0000

    [ADD] adding create and connect operations to TFBS and Regulatory Interaction nodes

--- src/domain/database/regulatory_interaction/operations.py
+++ src/domain/database/regulatory_interaction/operations.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Any
 
 from rest_framework import status
 
-from data import RegulatoryInteraction
+from data import RegulatoryInteraction, Organism, Regulator, Gene, TFBS, Effector
 import domain.model_api as mapi
 from domain.database.effector import get_effector_by_id
 from domain.database.organism import get_organism_by_id
@@ -17,35 +17,56 @@ _HEADER = 'PRT'
 _ENTITY = 'RIN'
 
 
-lookup_queries = {'organism': get_organism_by_id, 'regulator': get_regulator_by_id,
-                  'gene': get_gene_by_id, 'tfbs': get_binding_site_by_id, 'effector': get_effector_by_id}
+# order matters - do not change dict order
+lookup_queries = {'organism': get_organism_by_id,
+                  'regulator': get_regulator_by_id,
+                  'gene': get_gene_by_id,
+                  'tfbs': get_binding_site_by_id,
+                  'effector': get_effector_by_id}
 
 
 def _validate_submitted_objects(interaction: dict):
+    objs = []
     for key, lookup in lookup_queries.items():
 
-        if key not in interaction:
-            continue
+        if key in interaction:
+            lookup_value = interaction[key]
+            obj = lookup(lookup_value)
+            if obj is None:
+                raise ProtrendException(detail=f'The submitted {lookup_value} protrend id for the property {key} '
+                                               f'was not found in the database.',
+                                        code='create or update error',
+                                        status=status.HTTP_400_BAD_REQUEST)
 
-        lookup_value = interaction[key]
-        obj = lookup(lookup_value)
-        if obj is None:
-            raise ProtrendException(detail=f'The submitted {lookup_value} protrend id for the property {key} '
-                                           f'was not found in the database.',
-                                    code='create or update error',
-                                    status=status.HTTP_400_BAD_REQUEST)
+            objs.append(obj)
+
+        else:
+            objs.append(None)
+
+    return objs
 
 
 def create_interactions(*interactions: Dict[str, Any]) -> List[RegulatoryInteraction]:
     """
     Create interactions into the database
     """
+    submitted_objs = []
     for interaction in interactions:
-        _validate_submitted_objects(interaction)
+        submitted_obj = _validate_submitted_objects(interaction)
+        submitted_objs.append(submitted_obj)
 
     interactions = _validate_args_by_interaction_hash(args=interactions, node_cls=RegulatoryInteraction,
                                                       header=_HEADER, entity=_ENTITY)
-    return mapi.create_objects(RegulatoryInteraction, *interactions)
+
+    objs = mapi.create_objects(RegulatoryInteraction, *interactions)
+
+    for obj, submitted_obj in zip(objs, submitted_objs):
+        organism, regulator, gene, tfbs, effector = submitted_obj
+        create_interaction_relationships(interaction=obj,
+                                         organism=organism, regulator=regulator, gene=gene, tfbs=tfbs,
+                                         effector=effector)
+
+    return objs
 
 
 def delete_interactions(*interactions: RegulatoryInteraction):
@@ -59,28 +80,23 @@ def create_interaction(**kwargs) -> RegulatoryInteraction:
     """
     Create a given interaction into the database according to the parameters
     """
-    _validate_submitted_objects(kwargs)
+    organism, regulator, gene, tfbs, effector = _validate_submitted_objects(kwargs)
     kwargs = _validate_kwargs_by_interaction_hash(kwargs=kwargs, node_cls=RegulatoryInteraction,
                                                   header=_HEADER, entity=_ENTITY)
-    return mapi.create_object(RegulatoryInteraction, **kwargs)
+    obj = mapi.create_object(RegulatoryInteraction, **kwargs)
+    create_interaction_relationships(interaction=obj,
+                                     organism=organism, regulator=regulator, gene=gene, tfbs=tfbs, effector=effector)
+    return obj
 
 
-def update_interaction(interaction: RegulatoryInteraction, **kwargs) -> RegulatoryInteraction:
+def update_interaction(_: RegulatoryInteraction, **kwargs) -> RegulatoryInteraction:
     """
     Update the interaction into the database according to the parameters
     """
-    if 'protrend_id' in kwargs:
-        raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
-                                code='create or update error',
-                                status=status.HTTP_400_BAD_REQUEST)
-
-    _validate_submitted_objects(kwargs)
-
-    # the protrend hash must always be regenerated
-    kwargs = _validate_kwargs_by_interaction_hash(kwargs=kwargs, node_cls=RegulatoryInteraction,
-                                                  header=_HEADER, entity=_ENTITY)
-    kwargs.pop('protrend_id')
-    return mapi.update_object(interaction, **kwargs)
+    raise ProtrendException(detail=f'RegulatoryInteraction update operation is not supported. '
+                                   f'Please create a new interaction',
+                            code='create or update error',
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 def delete_interaction(interaction: RegulatoryInteraction) -> RegulatoryInteraction:
@@ -88,3 +104,54 @@ def delete_interaction(interaction: RegulatoryInteraction) -> RegulatoryInteract
     Delete the interaction from the database
     """
     return mapi.delete_object(interaction)
+
+
+def create_interaction_relationships(interaction: RegulatoryInteraction,
+                                     organism: Organism,
+                                     regulator: Regulator,
+                                     gene: Gene,
+                                     tfbs: TFBS,
+                                     effector: Effector):
+    """
+    Create a relationship between interactions and organism regulator gene tfbs effector
+    """
+    # organism → regulator; gene; interaction
+    mapi.create_relationship(source_obj=organism, target='regulator', target_obj=regulator)
+    mapi.create_relationship(source_obj=organism, target='gene', target_obj=gene)
+    mapi.create_relationship(source_obj=organism, target='regulatory_interaction', target_obj=interaction)
+
+    # regulator → organism; gene; tfbs; effector; interaction
+    mapi.create_relationship(source_obj=regulator, target='organism', target_obj=organism)
+    mapi.create_relationship(source_obj=regulator, target='gene', target_obj=gene)
+    if tfbs is not None:
+        mapi.create_relationship(source_obj=regulator, target='tfbs', target_obj=tfbs)
+    if effector is not None:
+        mapi.create_relationship(source_obj=regulator, target='effector', target_obj=effector)
+    mapi.create_relationship(source_obj=regulator, target='regulatory_interaction', target_obj=interaction)
+
+    # gene → organism; regulator; tfbs; interaction
+    mapi.create_relationship(source_obj=gene, target='organism', target_obj=organism)
+    mapi.create_relationship(source_obj=gene, target='regulator', target_obj=regulator)
+    if tfbs is not None:
+        mapi.create_relationship(source_obj=gene, target='tfbs', target_obj=tfbs)
+    mapi.create_relationship(source_obj=gene, target='regulatory_interaction', target_obj=interaction)
+
+    # tfbs → regulator; gene; interaction
+    if tfbs is not None:
+        mapi.create_relationship(source_obj=tfbs, target='regulator', target_obj=regulator)
+        mapi.create_relationship(source_obj=tfbs, target='gene', target_obj=gene)
+        mapi.create_relationship(source_obj=tfbs, target='regulatory_interaction', target_obj=interaction)
+
+    # effector → regulator; interaction
+    if effector is not None:
+        mapi.create_relationship(source_obj=effector, target='regulator', target_obj=regulator)
+        mapi.create_relationship(source_obj=effector, target='regulatory_interaction', target_obj=interaction)
+
+    # interaction → organism; regulator; gene; tfbs; effector
+    mapi.create_relationship(source_obj=interaction, target='data_organism', target_obj=organism)
+    mapi.create_relationship(source_obj=interaction, target='data_regulator', target_obj=regulator)
+    mapi.create_relationship(source_obj=interaction, target='data_gene', target_obj=gene)
+    if tfbs is not None:
+        mapi.create_relationship(source_obj=interaction, target='data_tfbs', target_obj=tfbs)
+    if effector is not None:
+        mapi.create_relationship(source_obj=interaction, target='data_effector', target_obj=effector)
--- src/domain/database/tfbs/operations.py
+++ src/domain/database/tfbs/operations.py
@@ -2,12 +2,11 @@ from typing import List, Dict, Any
 
 from rest_framework import status
 
-from data import TFBS
 import domain.model_api as mapi
+from data import TFBS, Organism
 from domain.database._validate import _validate_args_by_site_hash, _validate_kwargs_by_site_hash
 from domain.database.organism import get_organism_by_id
 from exceptions import ProtrendException
-
 
 _HEADER = 'PRT'
 _ENTITY = 'TBS'
@@ -17,6 +16,7 @@ def create_binding_sites(*binding_sites: Dict[str, Any]) -> List[TFBS]:
     """
     Create binding_sites into the database
     """
+    organisms = []
     for site in binding_sites:
         organism = site['organism']
         obj = get_organism_by_id(organism)
@@ -25,9 +25,15 @@ def create_binding_sites(*binding_sites: Dict[str, Any]) -> List[TFBS]:
                                            f'was not found in the database.',
                                     code='create or update error',
                                     status=status.HTTP_400_BAD_REQUEST)
+        organisms.append(obj)
 
     binding_sites = _validate_args_by_site_hash(args=binding_sites, node_cls=TFBS, header=_HEADER, entity=_ENTITY)
-    return mapi.create_objects(TFBS, *binding_sites)
+    objs = mapi.create_objects(TFBS, *binding_sites)
+
+    for obj, organism_obj in zip(objs, organisms):
+        create_binding_site_relationships(binding_site=obj, organism=organism_obj)
+
+    return objs
 
 
 def delete_binding_sites(*binding_sites: TFBS):
@@ -42,39 +48,27 @@ def create_binding_site(**kwargs) -> TFBS:
     Create a given binding_site into the database according to the parameters
     """
     organism = kwargs['organism']
-    obj = get_organism_by_id(organism)
-    if obj is None:
+    organism_obj = get_organism_by_id(organism)
+    if organism_obj is None:
         raise ProtrendException(detail=f'The submitted {organism} protrend id for the property organism '
                                        f'was not found in the database.',
                                 code='create or update error',
                                 status=status.HTTP_400_BAD_REQUEST)
 
     kwargs = _validate_kwargs_by_site_hash(kwargs=kwargs, node_cls=TFBS, header=_HEADER, entity=_ENTITY)
-    return mapi.create_object(TFBS, **kwargs)
+    obj = mapi.create_object(TFBS, **kwargs)
+    create_binding_site_relationships(binding_site=obj, organism=organism_obj)
+    return obj
 
 
-def update_binding_site(binding_site: TFBS, **kwargs) -> TFBS:
+def update_binding_site(_: TFBS, **kwargs):
     """
     Update the binding_site into the database according to the parameters
     """
-    if 'protrend_id' in kwargs:
-        raise ProtrendException(detail=f'protrend_id read-only attribute cannot be altered',
-                                code='create or update error',
-                                status=status.HTTP_400_BAD_REQUEST)
-
-    if 'organism' in kwargs:
-        organism = kwargs['organism']
-        obj = get_organism_by_id(organism)
-        if obj is None:
-            raise ProtrendException(detail=f'The submitted {organism} protrend id for the property organism '
-                                           f'was not found in the database.',
-                                    code='create or update error',
-                                    status=status.HTTP_400_BAD_REQUEST)
-
-    # the protrend hash must always be regenerated
-    kwargs = _validate_kwargs_by_site_hash(kwargs=kwargs, node_cls=TFBS, header=_HEADER, entity=_ENTITY)
-    kwargs.pop('protrend_id')
-    return mapi.update_object(binding_site, **kwargs)
+    raise ProtrendException(detail=f'TFBS update operation is not supported. '
+                                   f'Please create a new binding site',
+                            code='create or update error',
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 def delete_binding_site(binding_site: TFBS) -> TFBS:
@@ -82,3 +76,11 @@ def delete_binding_site(binding_site: TFBS) -> TFBS:
     Delete the binding_site from the database
     """
     return mapi.delete_object(binding_site)
+
+
+def create_binding_site_relationships(binding_site: TFBS, organism: Organism):
+    """
+    Create a relationship between binding_site and organism
+    """
+    mapi.create_relationship(source_obj=binding_site, target='data_organism', target_obj=organism)
+    mapi.create_relationship(source_obj=organism, target='tfbs', target_obj=binding_site)
