77ebd9ebea128c49e7b3001ac7f991203f908d82
cruz-f <fernandocruz184@gmail.com>
2022-02-07 14:34:53 +0000

    [FIX] moving set list from outside of source to source; fix uniqueness in the set list object

--- src/set_list.py
+++ src/set_list.py
@@ -8,37 +8,43 @@ T = TypeVar('T')
 class SetList(UserList, List[T]):
 
     def __init__(self, sequence: Union[Iterator, Sequence] = None, key: str = 'protrend_id'):
-
-        super().__init__()
-
-        self.key = key
-
         if sequence is None:
             sequence = []
 
-        self._add_elements(sequence)
+        super().__init__()
 
-    @property
-    def keys(self):
-        return [getattr(obj, self.key) for obj in self.data]
-
-    def _add_element(self, element: Any):
-        keys = self.keys
-        if element not in keys:
-            self.data.append(element)
-
-    def _add_elements(self, sequence: Sequence):
-        keys = self.keys
+        self._unique = []
+        self.key = key
 
         for element in sequence:
-            if element not in keys:
-                self.data.append(element)
+            self._add_element(element)
+
+    def _get_element_key(self, element: Any):
+        if self.key:
+            return getattr(element, self.key)
+
+        hash(element)
+        return element
+
+    def _add_element(self, element: Any):
+        key = self._get_element_key(element)
+
+        if key not in self._unique:
+            self._unique.append(key)
+            self.data.append(element)
+
+    def _insert_element(self, i: int, element: Any):
+        key = self._get_element_key(element)
+
+        if key not in self._unique:
+            self._unique[i] = element
+            self.data[i] = element
 
     def __setitem__(self, i, item):
-        if item not in self.keys:
-            self.data[i] = item
+        self._insert_element(i, item)
 
     def __delitem__(self, i):
+        del self._unique[i]
         del self.data[i]
 
     def __add__(self, other: Sequence):
@@ -56,31 +62,18 @@ class SetList(UserList, List[T]):
         return new_instance
 
     def __iadd__(self, other: Sequence):
-
-        self._add_elements(other)
-
+        self.extend(other)
         return self
 
     def append(self, item):
         self._add_element(item)
 
     def insert(self, i, item):
-        if item not in self.keys:
-            self.data.insert(i, item)
+        self._insert_element(i, item)
 
     def copy(self) -> 'SetList':
         return self.__class__(self)
 
     def extend(self, other: Sequence):
-        self._add_elements(other)
-
-    def take_all(self):
-        return self.data
-
-    def take_first(self):
-        if self.data:
-            return self.data[0]
-
-    def take_last(self):
-        if self.data:
-            return self.data[-1]
+        for element in other:
+            self._add_element(element)
