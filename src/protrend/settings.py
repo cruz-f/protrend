12fc5fe1f8447bbb37ddb95d70c237a517157240
cruz-f <fernandocruz184@gmail.com>
2022-02-16 16:50:39 +0000

    [ADD] development settings

--- src/protrend/settings.py
+++ src/protrend/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 ]
 
 # the static root should be only used in production.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
