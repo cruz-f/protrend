971cd9dadab3910be7e619a5b16dfe70df6a56f3
cruz-f <fernandocruz184@gmail.com>
2022-02-17 17:05:59 +0000

    [ADD] adding the lazy queries to heavily populated objects in the database; [FIX] simplifying the queries modules, as most of the queries were generic and thus not being used elsewhere

--- src/domain/database/operon/queries.py
+++ src/domain/database/operon/queries.py
@@ -1,91 +1,12 @@
 from typing import List
 
-from data import Operon, Organism, Gene, Source
 import domain.model_api as mapi
-from data.relationships import SourceRelationship
+from data import Operon
 
 
-def get_operons() -> List[Operon]:
+def get_lazy_operons() -> List[Operon]:
     """
     Get operons list from database
     """
-    return mapi.get_objects(Operon)
-
-
-def filter_operons(*args, **kwargs) -> List[Operon]:
-    """
-    Get and filter operons from database
-    """
-    return mapi.filter_objects(Operon, *args, **kwargs)
-
-
-def order_by_operons(*fields) -> List[Operon]:
-    """
-    Get and order by operons from database
-    """
-    return mapi.order_by_objects(Operon, *fields)
-
-
-def order_operons_by_id() -> List[Operon]:
-    """
-    Get and order by operons by protrend identifier from database
-    """
-    return mapi.order_by_objects(Operon, 'protrend_id')
-
-
-def get_operon(**kwargs) -> Operon:
-    """
-    Get a given operon from database
-    """
-    return mapi.get_object(Operon, **kwargs)
-
-
-def get_operon_by_id(protrend_id: str) -> Operon:
-    """
-    Get a given operon from database using the protrend identifier
-    """
-    return mapi.get_object(Operon, protrend_id=protrend_id)
-
-
-def get_last_operon() -> Operon:
-    """
-    Get the last operon from database using the protrend identifier
-    """
-    return mapi.get_last_object(Operon, 'protrend_id')
-
-
-def get_operon_sources(protrend_id: str) -> List[Source]:
-    """
-    Get sources connected with this operon
-    """
-    obj = get_operon_by_id(protrend_id)
-    return mapi.get_related_objects(obj, 'data_source')
-
-
-def get_operon_sources_relationships(protrend_id: str) -> List[SourceRelationship]:
-    """
-    Get sources relationships connected with this operon
-    """
-    obj = get_operon_by_id(protrend_id)
-    sources = mapi.get_related_objects(obj, 'data_source')
-    relationships = []
-    for source in sources:
-        source_relationships = mapi.get_relationships(source=obj, rel='data_source', target=source)
-        relationships.extend(source_relationships)
-    return relationships
-
-
-def get_operon_organisms(protrend_id: str) -> List[Organism]:
-    """
-    Get organism connected with this operon
-    """
-    obj = get_operon_by_id(protrend_id)
-    return mapi.get_related_objects(obj, 'organism')
-
-
-def get_operon_genes(protrend_id: str) -> List[Gene]:
-    """
-    Get genes connected with this operon
-    """
-    obj = get_operon_by_id(protrend_id)
-    return mapi.get_related_objects(obj, 'gene')
+    lazy_properties = ['protrend_id', 'operon_db_id', 'name', 'function', 'genes']
+    return mapi.get_lazy_objects(Operon, lazy_properties)
--- src/domain/database/tfbs/__init__.py
+++ src/domain/database/tfbs/__init__.py
@@ -1,10 +1,4 @@
-from .queries import (filter_binding_sites,
-                      get_binding_site,
-                      get_binding_site_by_id,
-                      get_binding_sites,
-                      get_last_binding_site,
-                      order_by_binding_sites,
-                      order_binding_sites_by_id)
+from .queries import (get_lazy_binding_sites)
 from .operations import (create_binding_site,
                          create_binding_sites,
                          delete_binding_site,
