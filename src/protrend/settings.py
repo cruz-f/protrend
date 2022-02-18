ec9e9814ceef4b268a6278390a031df00fc05a93
cruz-f <fernandocruz184@gmail.com>
2022-02-18 10:24:47 +0000

    [FIX] reducing page size

--- src/protrend/settings.py
+++ src/protrend/settings.py
@@ -168,7 +168,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # django-rest framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 50,
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
