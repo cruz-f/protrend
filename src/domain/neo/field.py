04d586575e230be51b44655a7c4046ceade2e9e9
cruz-f <fernandocruz184@gmail.com>
2022-03-10 10:52:59 +0000

    [FIX] documenting the data descriptors under the fields module

--- src/domain/neo/field.py
+++ src/domain/neo/field.py
@@ -1,6 +1,17 @@
+# -----------------------------------------------
+# NeoNode DESCRIPTORS
+# -----------------------------------------------
 class DefaultField:
 
     def __init__(self, name, default=None):
+        """
+        Default field stores a default value for the NeoNode instance attribute lookup.
+        At the first lookup if the attribute value is not present in the instance dict, the default field adds the
+        default value received upon instantiation and class formulation
+
+        :param name:
+        :param default:
+        """
         self.name = name
         self.default = default
 
@@ -27,6 +38,11 @@ class DefaultField:
 class NodeField:
 
     def __init__(self, name):
+        """
+        It is a simple data-descriptor that will retrieve NeoNode instance attributes, namely the NeoNode fields
+
+        :param name:
+        """
         self.name = name
 
     def __set_name__(self, owner, name):
@@ -47,9 +63,20 @@ class NodeField:
 
 class NodeLinkField:
 
-    def __init__(self, name, link_cls):
+    def __init__(self, name, target_cls):
+        """
+        It is simple data-descriptor that will retrieve the NeoNode instance relationships.
+        Each relationship is composed of a list of other NeoNode instances
+        which hold the target nodes connected to the source node
+
+        The data-descriptor is instantiated with the target NeoNode class formulation. This class can be used to create
+        the NeoNode instances of the connected nodes upon setting the node attribute.
+
+        :param name:
+        :param target_cls:
+        """
         self.name = name
-        self.link_cls = link_cls
+        self.target_cls = target_cls
 
     def __set_name__(self, owner, name):
         self.name = name
@@ -67,15 +94,23 @@ class NodeLinkField:
         if not value:
             value = []
 
-        values = [self.link_cls(**kwargs) for kwargs in value]
+        values = [self.target_cls(**kwargs) for kwargs in value]
         instance.__dict__[self.name] = values
 
 
 class NodeRelationshipField:
 
-    def __init__(self, name, rel_cls):
+    def __init__(self, name, relationship_cls):
+        """
+        It is a simple data-descriptor but for relationships. It is similar to the NodeLinkFields,
+        but this data-descriptor is used to register the relationship attribute in the NeoNode instances of the
+        connected nodes.
+        This data-descriptor is responsible for getting and setting the properties of the relationships
+        :param name:
+        :param relationship_cls:
+        """
         self.name = name
-        self.rel_cls = rel_cls
+        self.relationship_cls = relationship_cls
 
     def __set_name__(self, owner, name):
         self.name = name
@@ -93,5 +128,5 @@ class NodeRelationshipField:
         if not value:
             value = {}
 
-        value = self.rel_cls(**value)
+        value = self.relationship_cls(**value)
         instance.__dict__[self.name] = value
