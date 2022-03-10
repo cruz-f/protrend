bf27f351bf0af816e0749655b920f3818d0eb2f4
cruz-f <fernandocruz184@gmail.com>
2022-03-10 09:06:17 +0000

    [FIX] renaming parameters from label to variable to be cypher query compliance [ADD] adding the default field during class reconstruction for the fields', targets' and relationships' attributes

--- src/domain/neo/node.py
+++ src/domain/neo/node.py
@@ -1,6 +1,6 @@
 from typing import List, Type, Union
 
-from .field import NodeField, NodeRelationshipField, NodeLinkField
+from .field import NodeField, NodeRelationshipField, NodeLinkField, DefaultField
 
 
 class BaseNode:
@@ -9,14 +9,39 @@ class BaseNode:
             setattr(self, attr, kwarg)
 
     def __str__(self):
-        if hasattr(self, 'fields'):
-            fields = ', '.join(f'{field}: {getattr(self, field)}' for field in self.fields)
+        if hasattr(self, 'fields_'):
+            fields = ', '.join(f'{field}: {getattr(self, field)}' for field in self.fields_)
             return f'{{{fields}}}'
 
         return self.__class__.__name__
 
     def __repr__(self):
         return self.__str__()
+
+    def __getattr__(self, item):
+        raise AttributeError(f'{item} attribute not found in BaseNode')
+
+    def add(self, other):
+        if hasattr(self, 'fields_') and hasattr(other, 'fields_'):
+
+            for field in other.fields_:
+                if field not in self.fields_:
+                    value = getattr(other, field)
+                    setattr(self, field, value)
+                    self.fields_.append(field)
+
+        if hasattr(other, 'targets_'):
+
+            if not hasattr(self, 'targets_'):
+                setattr(self, 'targets_', [])
+
+            for target in other.targets_:
+                if target not in self.targets_:
+                    value = getattr(other, target)
+                    setattr(self, target, value)
+                    self.targets_.append(target)
+
+        return self
 
 
 class NodeMeta(type):
@@ -25,43 +50,42 @@ class NodeMeta(type):
                 bases,
                 namespace,
                 fields: List[str] = None,
-                link: str = None,
-                link_fields: List[str] = None,
-                rel_fields: List[str] = None):
-        # the new node class
+                target: str = None,
+                target_fields: List[str] = None,
+                relationship_fields: List[str] = None):
+        # the new source class
         cls = super(NodeMeta, mcs).__new__(mcs, name, bases, namespace)
 
-        # the new node class fields
-        if fields is not None:
+        # the new source class fields
+        if fields:
             for field in fields:
                 setattr(cls, field, NodeField(field))
-            setattr(cls, 'fields', fields)
+            setattr(cls, 'fields_', DefaultField('fields_', fields))
 
-        # a new node class is created for a given link (target node)
-        if link is not None:
+        # a new source class is created for a given target (target source)
+        if target:
             link_cls = super(NodeMeta, mcs).__new__(mcs, name, bases, namespace)
-            setattr(cls, link, NodeLinkField(link, link_cls))
-            setattr(cls, 'link_fields', link_fields)
+            setattr(cls, target, NodeLinkField(target, link_cls))
+            setattr(cls, 'targets_', DefaultField('targets_', [target]))
 
             rel_cls = super(NodeMeta, mcs).__new__(mcs, name, bases, namespace)
-            setattr(link_cls, 'relationship', NodeRelationshipField('relationship', rel_cls))
-            setattr(cls, 'rel_fields', rel_fields)
+            setattr(link_cls, 'relationship_', NodeRelationshipField('relationship_', rel_cls))
 
-            # the new node class fields for the link
-            if link_fields is not None:
+            # the new source class fields for the target
+            if target_fields:
 
-                for field in link_fields:
+                for field in target_fields:
                     setattr(link_cls, field, NodeField(field))
 
-                setattr(link_cls, 'fields', link_fields)
+                setattr(link_cls, 'fields_', DefaultField('fields_', target_fields))
 
-            # the new relationship class fields for the link
-            if rel_fields is not None:
+            # the new relationship class fields for the target
+            if relationship_fields:
 
-                for field in rel_fields:
+                for field in relationship_fields:
                     setattr(rel_cls, field, NodeField(field))
 
-                setattr(rel_cls, 'fields', rel_fields)
+                setattr(rel_cls, 'fields_', DefaultField('fields_', relationship_fields))
 
         return cls
 
@@ -70,14 +94,15 @@ class NodeMeta(type):
 
 
 def node_factory(fields: List[str] = None,
-                 link: str = None,
-                 link_fields: List[str] = None,
-                 rel_fields: List[str] = None) -> Union[type, Type[BaseNode]]:
+                 target: str = None,
+                 target_fields: List[str] = None,
+                 relationship_fields: List[str] = None) -> Union[type, Type[BaseNode]]:
+
     cls = NodeMeta(name='Node',
                    bases=(BaseNode,),
                    namespace={},
                    fields=fields,
-                   link=link,
-                   link_fields=link_fields,
-                   rel_fields=rel_fields)
+                   target=target,
+                   target_fields=target_fields,
+                   relationship_fields=relationship_fields)
     return cls
