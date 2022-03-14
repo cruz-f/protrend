f67414be58a0105539f7f76206a05e7233e7840c
cruz-f <fernandocruz184@gmail.com>
2022-03-14 10:33:11 +0000

    [ADD] using the website router

--- src/interfaces/website/urls.py
+++ src/interfaces/website/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 
 from interfaces.website import views
 
-urlpatterns = [path(r'', views.index, name='home'),
-               path('organisms', views.OrganismsView.as_view(), name='organisms'),
-               path('organisms/<str:protrend_id>', views.OrganismView.as_view(), name='organism'),
-               path('regulators/<str:protrend_id>', views.fake_view, name='regulator'),
-               path('genes/<str:protrend_id>', views.fake_view, name='gene'),
-               path('bindings/<str:protrend_id>', views.fake_view, name='binding'),
-               path('interactions/<str:protrend_id>', views.fake_view, name='interaction')]
+import interfaces.website.views as website_views
+from .router import WebsiteRouter
+
+
+# Create a router and register our class-based views.
+router = WebsiteRouter(r'')
+router.register(r'organisms', list_view=website_views.OrganismsView, detail_view=website_views.OrganismView)
+
+urlpatterns = [
+    path(r'', include(router.urls)),
+    path('regulators/<str:protrend_id>', views.fake_view, name='regulator'),
+    path('genes/<str:protrend_id>', views.fake_view, name='gene'),
+    path('bindings/<str:protrend_id>', views.fake_view, name='binding'),
+    path('interactions/<str:protrend_id>', views.fake_view, name='interaction')]
