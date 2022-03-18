728d3535b623f0c7bdf19e404964f9b48b760e2b
cruz-f <fernandocruz184@gmail.com>
2022-03-18 17:14:38 +0000

    [ADD] adding sequence logos application that allows the generation of sequence motif logos with logo maker package; adding pwm matrix generation

---
+++ src/application/sequences/logo.py
@@ -0,0 +1,33 @@
+from io import StringIO
+from typing import List
+
+import logomaker as lm
+import pandas as pd
+
+
+def relative_frequency(*args, **kwargs):
+    kwargs['normalize'] = True
+    return pd.value_counts(*args, **kwargs)
+
+
+def make_pwm(sequences: List[str]) -> pd.DataFrame:
+    df = pd.DataFrame([list(seq) for seq in sequences])
+    pwm = df.apply(relative_frequency)
+    pwm = pwm.fillna(0)
+    pwm = pwm.transpose()
+    return pwm
+
+
+def make_motif_logo(pwm: pd.DataFrame) -> lm.Logo:
+    logo = lm.Logo(pwm, color_scheme='classic')
+    logo.ax.set_ylabel("nucleotide frequency", labelpad=-1)
+    return logo
+
+
+def make_motif_img(logo: lm.Logo) -> str:
+    img_data = StringIO()
+    logo.fig.savefig(img_data, format='svg')
+    img_data.seek(0)
+
+    data = img_data.getvalue()
+    return data
