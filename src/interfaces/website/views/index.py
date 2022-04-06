1ac20af52ea28352fa7dbb96b3b41da4abe4cfd1
cruz-f <fernandocruz184@gmail.com>
2022-04-06 08:57:32 +0100

    [FIX] fixing index view with active page

--- src/interfaces/website/views/index.py
+++ src/interfaces/website/views/index.py
@@ -7,6 +7,14 @@ class IndexView(generic.TemplateView):
     template_name = "website/index.html"
     view_name = 'home'
 
+    def get_context_data(self, **kwargs):
+        # the view context
+        # noinspection PyUnresolvedReferences
+        context = super().get_context_data(**kwargs)
+
+        context['active_page'] = 'home'
+        return context
+
 
 def fake_view(request, param=None):
     return render(request, "website/index.html", {'active_page': 'home'})
