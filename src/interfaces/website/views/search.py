5a735f42473fd9fea7f4ff6ef9f3defcb3ecd0f1
cruz-f <fernandocruz184@gmail.com>
2022-07-02 11:48:01 +0100

    [ADD] improving query results

--- src/interfaces/website/views/search.py
+++ src/interfaces/website/views/search.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 
@@ -22,34 +24,67 @@ FILTER_MAP = [
 ]
 
 
+def remove_text_prepositions(query):
+    query = query.replace('in', '')
+    query = query.replace('of', '')
+    query = query.replace('on', '')
+    query = query.replace('to', '')
+    query = query.replace('from', '')
+    query = query.replace('with', '')
+    query = query.replace('without', '')
+    query = query.replace('for', '')
+    query = query.replace('and', '')
+    query = query.replace('or', '')
+    query = query.replace('not', '')
+    query = query.replace('but', '')
+    query = query.replace('nor', '')
+    query = query.replace('yet', '')
+    query = query.replace('so', '')
+    query = query.replace('than', '')
+    query = query.replace('after', '')
+    query = query.replace('before', '')
+    query = query.replace('between', '')
+    query = query.replace('during', '')
+    query = query.replace('since', '')
+    query = query.replace('until', '')
+    return query
+
+
 @csrf_protect
 def search(request):
-    results = []
 
     params = request.GET
     if not params:
-        return render(request, "website/search.html", {'results': results,
+        return render(request, "website/search.html", {'results': [],
                                                        'query': '',
                                                        'active_page': 'search'})
 
     if 'q' not in params:
-        return render(request, "website/search.html", {'results': results,
+        return render(request, "website/search.html", {'results': [],
                                                        'query': '',
                                                        'active_page': 'search'})
 
     query = params['q']
     if not query:
-        return render(request, "website/search.html", {'results': results,
+        return render(request, "website/search.html", {'results': [],
                                                        'query': '',
                                                        'active_page': 'search'})
+    counts = defaultdict(int)
+    results = []
+    processed_query = remove_text_prepositions(query)
+    queries = set([processed_query] + processed_query.split(' '))
+    for query_ in queries:
+        for filter_ in FILTER_MAP:
+            model, fields = filter_
+            for field in fields:
+                kwargs = {f'{field}__contains': str(query_)}
+                objects = dpi.filter_objects(model, fields, **kwargs)
+                for object_ in objects:
+                    counts[object_.protrend_id] += 1
+                    if object_.protrend_id not in results:
+                        results.append(object_)
 
-    for filter_ in FILTER_MAP:
-        model, fields = filter_
-        for field in fields:
-            kwargs = {f'{field}__contains': str(query)}
-            objects = dpi.filter_objects(model, fields, **kwargs)
-            results.extend(objects)
-
+    results = sorted(results, key=lambda k: counts[k.protrend_id], reverse=True)
     return render(request, "website/search.html", {'results': results,
                                                    'query': query,
                                                    'active_page': 'search'})
