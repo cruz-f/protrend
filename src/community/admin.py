dbcec8712829160df18fc4117f443eec3de32eb9
cruz-f <fernandocruz184@gmail.com>
2022-07-22 11:03:40 +0100

    [FIX] minor fix to admin user

--- src/community/admin.py
+++ src/community/admin.py
@@ -52,27 +52,12 @@ class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
 
-    # # The fields to be used in displaying the User model.
-    # # These override the definitions on the base UserAdmin
-    # # that reference specific fields on auth.User.
-    # list_display = ('email', 'username', 'is_admin')
-    # list_filter = ('is_admin',)
-    # fieldsets = (
-    #     (None, {'fields': ('email', 'password')}),
-    #     ('Personal info', {'fields': ('username', )}),
-    #     ('Permissions', {'fields': ('is_admin',)}),
-    # )
-    # # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', 'email', 'first_name', 'last_name', 'institution', 'password1', 'password2'),
         }),
     )
-    # search_fields = ('email',)
-    # ordering = ('email',)
-    # filter_horizontal = ()
 
 
 # Now register the new UserAdmin...
